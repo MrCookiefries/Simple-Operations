e3362049aa06eff6fc50585101042235f0a6a6b6
Michael Copeland <cookiefries12@gmail.com>
2019-04-27 18:05:22 -0500

    Added the Nav for all pages, and finished the code for the tempature conversion.

--- tempatureConversion.js
+++ tempatureConversion.js
@@ -1,5 +1,5 @@
 function tempCalc() {
-  let tn1 = document.getElementById('firstTemp').value;
+  let tn1 = parseFloat(document.getElementById('firstTemp').value);
   let tt1 = document.getElementById('tempType1').value;
   let tt2 = document.getElementById('tempType2').value;
   if (tt1 === 'fahrenheit') {
@@ -10,7 +10,7 @@ function tempCalc() {
     } else if (tt2 === 'celsius') {
       tn1 = (tn1 - 32) * (5 / 9);
     } else {
-      tn1 = "Error in the code!";
+      tn1 = "Error in Fahrenheit being temp type one.";
     }
   } else if (tt1 === 'kelvin') {
     if (tt2 === 'kelvin') {
@@ -20,9 +20,18 @@ function tempCalc() {
     } else if (tt2 === 'celsius') {
       tn1 = tn1 - 273.15;
     } else {
-      tn1 = "Error in code!";
+      tn1 = "Error in Kelvin being temp type one.";
     }
-  } else if ('celsius') {
-    //Finish Temp Calculator
+  } else if (tt1 === 'celsius') {
+    if (tt2 === 'celsius') {
+      tn1 = tn1;
+    } else if (tt2 === 'fahrenheit') {
+      tn1 = (tn1 * (9 / 5)) + 32;
+    } else if (tt2 === 'kelvin') {
+      tn1 = tn1 + 273.15;
+    } else {
+      tn1 = "Error in Celsius being temp type one.";
+    }
   }
+  document.getElementById('tempResult').innerHTML = tn1;
 }
